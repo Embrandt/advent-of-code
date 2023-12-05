bdb0e99a0f11bfabdf770932b845bd57b7bb0989
Marcel Brandt <3mbrandt@gmail.com>
2023-12-05 14:47:01 +0100

    Day 3 part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,53 +1,114 @@
 import kotlin.math.max
 import kotlin.math.min
 
-data class Number(val lineIndex: Int, val internalRange: IntRange, val data: Int)
+data class Num(val lineIndex: Int, val internalRange: IntRange, val data: String)
+data class Symbol(val lineIndex: Int, val charIndex: Int, val data: Char)
 
 fun main() {
-    fun isPart(number: Number, coordinates: Array<String>): Boolean {
-        val maxAbove = max(0, number.lineIndex - 1)
-        val maxBelow = min(coordinates.size - 1, number.lineIndex + 1)
-        val maxLeft = max(0, number.internalRange.first - 1)
-        val maxRight = min(coordinates[0].length - 1, number.internalRange.last + 1)
+    fun symbolsAdjacentToNumber(
+        num: Num,
+        coordinates: Array<String>,
+        symbolsToConsider: (Char) -> Boolean
+    ): MutableList<Symbol> {
+        val maxAbove = max(0, num.lineIndex - 1)
+        val maxBelow = min(coordinates.size - 1, num.lineIndex + 1)
+        val maxLeft = max(0, num.internalRange.first - 1)
+        val maxRight = min(coordinates[0].length - 1, num.internalRange.last + 1)
         val occupied = "[^(\\d)|.]".toRegex()
-        val above = coordinates[maxAbove].substring(maxLeft, maxRight + 1)
-        val below = coordinates[maxBelow].substring(maxLeft, maxRight + 1)
-        val left = coordinates[number.lineIndex][maxLeft]
-        val right = coordinates[number.lineIndex][maxRight]
+        val above = coordinates[maxAbove]
+        val below = coordinates[maxBelow]
+        val left = coordinates[num.lineIndex][maxLeft]
+        val right = coordinates[num.lineIndex][maxRight]
 
         val adjacentCharSequence = StringBuilder()
-        if (number.lineIndex != 0) {
+        val adjacentSymbols: MutableList<Symbol> = mutableListOf()
+        if (num.lineIndex != 0) {
+            above.withIndex().filter { (index, char) -> index in maxLeft..maxRight && symbolsToConsider(char) }
+                .mapTo(adjacentSymbols) { (index, char) -> Symbol(maxAbove, index, char) }
+
             adjacentCharSequence.append(above)
         }
-        if (number.lineIndex != coordinates.size - 1) {
+        if (num.lineIndex != coordinates.size - 1) {
+            below.withIndex().filter { (index, char) -> index in maxLeft..maxRight && symbolsToConsider(char) }
+                .mapTo(adjacentSymbols) { (index, char) -> Symbol(maxBelow, index, char) }
             adjacentCharSequence.append(below)
         }
-        if (maxLeft != number.internalRange.first) {
+        if (maxLeft != num.internalRange.first) {
+            if (symbolsToConsider(left)) {
+                adjacentSymbols.add(Symbol(num.lineIndex, maxLeft, left))
+            }
             adjacentCharSequence.append(left)
         }
-        if (maxRight != number.internalRange.last) {
+        if (maxRight != num.internalRange.last) {
+            if (symbolsToConsider(right)) {
+                adjacentSymbols.add(Symbol(num.lineIndex, maxRight, right))
+            }
             adjacentCharSequence.append(right)
         }
-        return occupied.containsMatchIn(adjacentCharSequence)
+
+        occupied.containsMatchIn(adjacentCharSequence)
+        return adjacentSymbols
+    }
+
+    fun isPart(num: Num, coordinates: Array<String>): Boolean {
+        return symbolsAdjacentToNumber(
+            num,
+            coordinates
+        ) { character -> character !in ".\n" && !character.isDigit() }
+            .isNotEmpty()
     }
 
     fun part1(input: List<String>): Int {
         val toArray = input.toTypedArray()
         val regex = "(\\d+)".toRegex()
 
-        return input.withIndex().flatMap { line -> regex.findAll(line.value).map { match -> Number(line.index, match.range, match.value.toInt()) }.filter { isPart(it, toArray) } }
-                .map { it.data }.toList().sum()
+        return input
+            .withIndex()
+            .flatMap { line ->
+                regex.findAll(line.value)
+                    .map { match ->
+                        Num(line.index, match.range, match.value)
+                    }
+            }
+            .filter { isPart(it, toArray) }
+            .map { it.data.toInt() }
+            .toList()
+            .sum()
     }
 
+    fun mapSymbolsToNumbers(number: Num, coordinates: Array<String>, symbolMap: MutableMap<Symbol, MutableList<Int>>) {
+        symbolsAdjacentToNumber(number, coordinates) { it == '*' }
+            .forEach { symbol ->
+                val numberList = symbolMap.getOrDefault(symbol, mutableListOf())
+                numberList.add(number.data.toInt())
+                symbolMap[symbol] = numberList
+            }
+    }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val toArray = input.toTypedArray()
+        val regex = "(\\d+)".toRegex()
+        val symbolMap = mutableMapOf<Symbol, MutableList<Int>>()
+        input
+            .withIndex()
+            .flatMap { line ->
+                regex.findAll(line.value)
+                    .map { match ->
+                        Num(line.index, match.range, match.value)
+                    }
+            }
+            .forEach { number -> mapSymbolsToNumbers(number, toArray, symbolMap) }
+        return symbolMap
+            .filter { (_, value) -> value.size == 2 }
+            .map { (_, value) -> value[0] * value[1] }
+            .sum()
+
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
     check(part1(testInput) == 4361)
-//    check(part2(testInput) == 467835)
+    check(part2(testInput) == 467835)
 
     val input = readInput("Day03")
     part1(input).println()
