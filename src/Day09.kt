617c0174e8794779d166b949bf40f49e44993ab8
Marcel Brandt <3mbrandt@gmail.com>
2023-12-10 07:33:59 +0100

    Day 9 part 2 cleanup

--- src/Day09.kt
+++ src/Day09.kt
@@ -11,25 +11,14 @@ fun main() {
         return pyramid.fold(0) { prediction: Long, list -> prediction + list.last }
     }
 
-    fun extrapolateBackWards(valueHistory: List<Long>): Long {
-        var lastCalculation = valueHistory
-        val pyramid = mutableListOf(lastCalculation)
-        while (lastCalculation.size != 1) {
-            val reducedList = lastCalculation.zipWithNext { a, b -> b - a }
-            if (reducedList.isNotEmpty()) pyramid.add(reducedList)
-            lastCalculation = reducedList
-        }
-        return pyramid.reversed().fold(0) { prediction: Long, list -> list.first - prediction }
-    }
-
     fun part1(input: List<String>): Int {
         val result = input.map { it.toNumberList() }
         return result.sumOf { extrapolate(it) }.toInt()
     }
 
     fun part2(input: List<String>): Int {
-        val result = input.map { it.toNumberList() }
-        return result.sumOf { extrapolateBackWards(it) }.toInt()
+        val result = input.map { it.toNumberList().reversed() }
+        return result.sumOf { extrapolate(it) }.toInt()
     }
 
     // test if implementation meets criteria from the description, like:
