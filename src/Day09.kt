198f5f150a01fe664ee8feef1ebc680914239a98
Marcel Brandt <3mbrandt@gmail.com>
2023-12-10 07:26:50 +0100

    Day 9 part 2

--- src/Day09.kt
+++ src/Day09.kt
@@ -8,7 +8,18 @@ fun main() {
             if (reducedList.isNotEmpty()) pyramid.add(reducedList)
             lastCalculation = reducedList
         }
-        return pyramid.reversed().fold(0) { prediction: Long, list -> prediction + list.last }
+        return pyramid.fold(0) { prediction: Long, list -> prediction + list.last }
+    }
+
+    fun extrapolateBackWards(valueHistory: List<Long>): Long {
+        var lastCalculation = valueHistory
+        val pyramid = mutableListOf(lastCalculation)
+        while (lastCalculation.size != 1) {
+            val reducedList = lastCalculation.zipWithNext { a, b -> b - a }
+            if (reducedList.isNotEmpty()) pyramid.add(reducedList)
+            lastCalculation = reducedList
+        }
+        return pyramid.reversed().fold(0) { prediction: Long, list -> list.first - prediction }
     }
 
     fun part1(input: List<String>): Int {
@@ -17,15 +28,16 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val result = input.map { it.toNumberList() }
+        return result.sumOf { extrapolateBackWards(it) }.toInt()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day09_test")
     val part1Result = part1(testInput)
     check(part1Result == 114) { "Part 1 wrong result: $part1Result" }
-//    val part2Result = part2(testInput)
-//    check(part2Result == 71503) { "Part 2 wrong result: $part2Result" }
+    val part2Result = part2(testInput)
+    check(part2Result == 2) { "Part 2 wrong result: $part2Result" }
 
     val input = readInput("Day09")
     part1(input).println()
