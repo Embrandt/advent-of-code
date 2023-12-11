2c45a70ea0f04c36cb4e5048b4352d6375db2da1
Marcel Brandt <3mbrandt@gmail.com>
2023-12-11 11:58:58 +0100

    Day 11 part 2

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,4 +1,6 @@
 import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 
 fun main() {
 
@@ -47,11 +49,15 @@ fun main() {
 
     }
 
-    fun part2(input: List<String>): Int {
-        val expandedRows = getExpandedUniverse(input, 10)
+    fun part2(input: List<String>, expandFactor: Int): Long {
+        val universe = getExpandedUniverse(input, 0)
+        val emptyRows = input.withIndex().filter { line -> !line.value.contains("#") }.map { it.index }
+        val emptyColumns = (0..<input[0].length).toList() - input.flatMap { s: String ->
+            s.withIndex().filter { it.value == '#' }.map { it.index }
+        }.toSet()
 
         val galaxies = buildList {
-            expandedRows.forEachIndexed { index, chars ->
+            universe.forEachIndexed { index, chars ->
                 chars.forEachIndexed { innerIndex, c ->
                     if (c == '#') add(Pair(index, innerIndex))
                 }
@@ -59,11 +65,15 @@ fun main() {
         }
 
         return galaxies.mapIndexed { index, galaxy ->
-            var sum = 0
+            var sum = 0L
             for (i in index + 1..<galaxies.size) {
                 val partnerGalaxy = galaxies[i]
                 val result = (partnerGalaxy.first - galaxy.first + abs(partnerGalaxy.second - galaxy.second))
-                sum += result
+                val add = emptyRows.count { deadSpace -> deadSpace in galaxy.first..partnerGalaxy.first }
+                val add2 = emptyColumns.count { deadSpace ->
+                    deadSpace in min(galaxy.second, partnerGalaxy.second)..max(galaxy.second, partnerGalaxy.second)
+                }
+                sum += result + (expandFactor - 1) * (add + add2)
             }
             sum
         }.sum()
@@ -73,11 +83,11 @@ fun main() {
     val testInput = readInput("Day11_test")
     val part1Result = part1(testInput)
     check(part1Result == 374) { "Part 1 wrong result: $part1Result" }
-//    val part2Result = part2(testInput)
-//    check(part2Result == 71503) { "Part 2 wrong result: $part2Result" }
+    val part2Result = part2(testInput, 10)
+    check(part2Result == 1030L) { "Part 2 wrong result: $part2Result" }
 
 
     val input = readInput("Day11")
     part1(input).println()
-    part2(input).println()
+    part2(input, 1000000).println()
 }
